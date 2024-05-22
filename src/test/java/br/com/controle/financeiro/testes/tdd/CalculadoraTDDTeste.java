941babf2144f5dc834b1ee1b4709747042cb5823
Samantha Almeida <samantha.kelly2@gmail.com>
2024-05-21 22:23:58 -0300

    ajuste em teste básico

--- src/test/java/br/com/controle/financeiro/testes/tdd/CalculadoraTDDTeste.java
+++ src/test/java/br/com/controle/financeiro/testes/tdd/CalculadoraTDDTeste.java
@@ -7,27 +7,12 @@ import org.junit.jupiter.api.Test;
 public class CalculadoraTDDTeste {
 
 	@Test
-	    void deveRetornarSomaCorreta_QuandoSomarDoisNumeros() {
-	        
-		    //Arrange
-		    CalculadoraTDD calculadora = new CalculadoraTDD();
-	        int = num1
-	        int = num2
-	        
-	        
-		    int resultado = calculadora.soma(2, 3);
-	        assertEquals(5, resultado);  // Esperado 2 + 3 = 5
-	    }
+	void deveRetornarSomaCorreta_QuandoSomarDoisNumeros() {
+
+		// Arrange
+		CalculadoraTDD calculadora = new CalculadoraTDD();
+
+		int resultado = calculadora.soma(2, 3);
+		assertEquals(5, resultado); // Esperado 2 + 3 = 5
+	}
 }
-
-            //Arrange
-			int num1 = 2;
-			int num2 = 3;
-			Calculadora calculadoraTDD = new CalculadoraTDD();
-			
-            // Act
-			int resultado = calculadora.soma(num1, num2);
-			
-		   //Assert
-			assertEquals(5, resultado);
-
